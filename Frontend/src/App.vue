5c10185e2ccc3b0cc5d170ed6def593c12c9e682
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2020-08-05 19:34:21 -0300

    Implementing ImagemResponsiva.vue

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -11,7 +11,7 @@
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
-          <img class="img-responsiva" :src="foto.url" :alt="foto.titulo" />
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
         </meu-painel>
       </li>
     </ul>
@@ -20,9 +20,12 @@
 
 <script>
 import Painel from "./components/shared/painel/Painel.vue";
+import ImagemResponsiva from "./components/shared/imagem-responsiva/ImagemResponsiva.vue";
+
 export default {
   components: {
     "meu-painel": Painel,
+    "imagem-responsiva": ImagemResponsiva
   },
 
   data() {
@@ -73,10 +76,6 @@ export default {
 
 .lista-fotos .lista-fotos-item {
   display: inline-block;
-}
-
-.img-responsiva {
-  width: 100%;
 }
 
 .filtro {
---
+++ Frontend/src/components/shared/imagem-responsiva/ImagemResponsiva.vue
@@ -0,0 +1,15 @@
+<template>
+    <img class="img-responsiva" :src="url" :alt="titulo" />
+</template>
+
+<script>
+export default {
+    props: ['url', 'titulo']
+}
+</script>
+
+<style scoped>
+.img-responsiva {
+  width: 100%;
+}
+</style>
