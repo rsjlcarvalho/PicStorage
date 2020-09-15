275f5ebe8390f76ceba61e2dc83945db745e3794
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2020-09-15 19:32:54 -0300

    Adding remove method;

--- Frontend/src/components/home/Home.vue
+++ Frontend/src/components/home/Home.vue
@@ -11,8 +11,8 @@
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
-          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
-          <meu-botao tipo="button" rotulo="REMOVER"/>
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
+          <meu-botao tipo="button" rotulo="REMOVER" @click.native="remove(foto)" />
         </meu-painel>
       </li>
     </ul>
@@ -22,13 +22,13 @@
 <script>
 import Painel from "../shared/painel/Painel.vue";
 import ImagemResponsiva from "../shared/imagem-responsiva/ImagemResponsiva.vue";
-import Botao from '../shared/botao/Botao.vue';
+import Botao from "../shared/botao/Botao.vue";
 
 export default {
   components: {
-    'meu-painel': Painel,
-    'imagem-responsiva': ImagemResponsiva,
-    'meu-botao': Botao
+    "meu-painel": Painel,
+    "imagem-responsiva": ImagemResponsiva,
+    "meu-botao": Botao,
   },
 
   data() {
@@ -47,6 +47,13 @@ export default {
       } else {
         return this.fotos;
       }
+    },
+  },
+
+  methods: {
+    remove(foto) {
+      if (confirm("Confirma operação?"))
+        alert("Remover a foto: " + foto.titulo);
     },
   },
 
