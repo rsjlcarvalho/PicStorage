b6339481be85fe68fbefac9ca01241344b309cc6
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2020-09-16 19:36:10 -0300

    Refactoring;

--- Frontend/src/components/home/Home.vue
+++ Frontend/src/components/home/Home.vue
@@ -12,7 +12,7 @@
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
           <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
-          <meu-botao tipo="button" rotulo="REMOVER" @click.native="remove(foto)" />
+          <meu-botao tipo="button" rotulo="REMOVER" @botaoAtivado="remove(foto)" />
         </meu-painel>
       </li>
     </ul>
@@ -52,8 +52,7 @@ export default {
 
   methods: {
     remove(foto) {
-      if (confirm("Confirma operação?"))
-        alert("Remover a foto: " + foto.titulo);
+      alert("Remover a foto: " + foto.titulo);
     },
   },
 
--- Frontend/src/components/shared/botao/Botao.vue
+++ Frontend/src/components/shared/botao/Botao.vue
@@ -1,10 +1,16 @@
 <template>
-  <button class="botao botao-perigo" :type="tipo">{{ rotulo }}</button>
+  <button @click="disparaAcao()" class="botao botao-perigo" :type="tipo">{{ rotulo }}</button>
 </template>
 
 <script>
 export default {
   props: ["tipo", "rotulo"],
+
+  methods: {
+    disparaAcao() {
+      if (confirm("Confirma operação?")) this.$emit("botaoAtivado");
+    },
+  },
 };
 </script>
 
