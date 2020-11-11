aef807a559908145f302e0b2a87a73821addc228
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2020-11-11 20:53:57 -0300

    Updating Cadastro.vue

--- Frontend/src/components/cadastro/Cadastro.vue
+++ Frontend/src/components/cadastro/Cadastro.vue
@@ -4,21 +4,21 @@
     <h1 class="centralizado">Cadastro</h1>
     <h2 class="centralizado"></h2>
 
-    <form>
+    <form @submit.prevent="grava()">
       <div class="controle">
         <label for="titulo">TÍTULO</label>
-        <input id="titulo" autocomplete="off">
+        <input id="titulo" autocomplete="off" @input="foto.titulo = $event.target.value" :value="foto.titulo">
       </div>
 
       <div class="controle">
         <label for="url">URL</label>
-        <input id="url" autocomplete="off">
+        <input id="url" autocomplete="off" @input="foto.url = $event.target.value" :value="foto.url">
         <imagem-responsiva/>
       </div>
 
       <div class="controle">
         <label for="descricao">DESCRIÇÃO</label>
-        <textarea id="descricao" autocomplete="off"></textarea>
+        <textarea id="descricao" autocomplete="off" @input="foto.descricao = $event.target.value" :value="foto.descricao"></textarea>
       </div>
 
       <div class="centralizado">
@@ -41,6 +41,25 @@ export default {
 
     'imagem-responsiva': ImagemResponsiva, 
     'meu-botao': Botao
+  },
+  data(){
+    return {
+      foto: {
+        titulo: '',
+        url: '',
+        descricao: ''
+      }
+    }
+  },
+  methods: {
+    grava() {
+      console.log('enviar dados para a API');
+      this.foto = {
+        titulo: '',
+        url: '',
+        descricao: ''
+      }
+    }
   }
 }
 
