3bc696bbe5bc2bc0933f88b453ff20a22389c1bc
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2020-12-01 20:45:53 -0300

    Sending data to the API

--- Frontend/src/components/cadastro/Cadastro.vue
+++ Frontend/src/components/cadastro/Cadastro.vue
@@ -50,9 +50,9 @@ export default {
   },
   methods: {
     grava() {
-      console.log('enviar dados para a API');
-      console.log(this.foto)
-      this.foto = new Foto()
+      
+      this.$http.post('http://localhost:3000/v1/fotos', this.foto)
+      .then(() => this.foto = new Foto(), err => console.log(err));
     }
   }
 }
