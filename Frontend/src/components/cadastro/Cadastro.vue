40e1e229dd976f241460640514f4b815dd09767c
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2021-02-04 20:07:59 -0300

    Validating fields;

--- Frontend/src/components/cadastro/Cadastro.vue
+++ Frontend/src/components/cadastro/Cadastro.vue
@@ -1,5 +1,4 @@
 <template>
-
   <div>
     <h1 class="centralizado">Cadastro</h1>
     <h2 class="centralizado">{{ foto.titulo }}</h2>
@@ -10,95 +9,125 @@
     <form @submit.prevent="grava()">
       <div class="controle">
         <label for="titulo">TÍTULO</label>
-        <input id="titulo" autocomplete="off" v-model.lazy="foto.titulo">
+        <input
+          name="titulo"
+          v-validate
+          data-vv-as="título"
+          data-vv-rules="required|min:3|max:30"
+          id="titulo"
+          autocomplete="off"
+          v-model="foto.titulo"
+        />
+        <span class="erro" v-show="errors.has('titulo')">{{
+          errors.first("titulo")
+        }}</span>
       </div>
 
       <div class="controle">
         <label for="url">URL</label>
-        <input id="url" autocomplete="off" v-model.lazy="foto.url">
-        <imagem-responsiva v-show="foto.url" :url="foto.url" :titulo="foto.titulo"/>
+        <input
+          name="url"
+          v-validate
+          data-vv-rules="required"
+          id="url"
+          autocomplete="off"
+          v-model="foto.url"
+        />
+        <span class="erro" v-show="errors.has('url')">{{
+          errors.first("url")
+        }}</span>
+        <imagem-responsiva
+          v-show="foto.url"
+          :url="foto.url"
+          :titulo="foto.titulo"
+        />
       </div>
 
       <div class="controle">
         <label for="descricao">DESCRIÇÃO</label>
-        <textarea id="descricao" autocomplete="off" v-model="foto.descricao"></textarea>
+        <textarea
+          id="descricao"
+          autocomplete="off"
+          v-model="foto.descricao"
+        ></textarea>
       </div>
 
       <div class="centralizado">
-        <meu-botao rotulo="GRAVAR" tipo="submit"/>
-        <router-link :to="{ name: 'home'}"><meu-botao rotulo="VOLTAR" tipo="button"/></router-link>
+        <meu-botao rotulo="GRAVAR" tipo="submit" />
+        <router-link :to="{ name: 'home' }"
+          ><meu-botao rotulo="VOLTAR" tipo="button"
+        /></router-link>
       </div>
-
     </form>
   </div>
 </template>
 
 <script>
-
-import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
-import Botao from '../shared/botao/Botao.vue';
-import Foto from '../../domain/foto/Foto';
+import ImagemResponsiva from "../shared/imagem-responsiva/ImagemResponsiva.vue";
+import Botao from "../shared/botao/Botao.vue";
+import Foto from "../../domain/foto/Foto";
 import FotoService from "../../domain/foto/FotoService";
 
 export default {
-
   components: {
-
-    'imagem-responsiva': ImagemResponsiva, 
-    'meu-botao': Botao
+    "imagem-responsiva": ImagemResponsiva,
+    "meu-botao": Botao,
   },
-  data(){
+  data() {
     return {
       foto: new Foto(),
-      id: this.$route.params.id
-    }
+      id: this.$route.params.id,
+    };
   },
   methods: {
     grava() {
-      
-      this.service
-        .cadastra(this.foto)
-        .then(() => { 
-          if(this.id) this.$router.push({ name: 'home' });
-          this.foto = new Foto();
-          }, err => console.log(err));
-    }
+      this.$validator.validateAll().then((success) => {
+        if (success) {
+          this.service.cadastra(this.foto).then(
+            () => {
+              if (this.id) this.$router.push({ name: "home" });
+              this.foto = new Foto();
+            },
+            (err) => console.log(err)
+          );
+        }
+      });
+    },
   },
   created() {
     this.service = new FotoService(this.$resource);
 
-    if(this.id) {
-      this.service
-      .busca(this.id)
-      .then(foto => this.foto = foto);
+    if (this.id) {
+      this.service.busca(this.id).then((foto) => (this.foto = foto));
     }
-  }
-}
-
+  },
+};
 </script>
 <style scoped>
+.centralizado {
+  text-align: center;
+}
+.controle {
+  font-size: 1.2em;
+  margin-bottom: 20px;
+}
+.controle label {
+  display: block;
+  font-weight: bold;
+}
 
-  .centralizado {
-    text-align: center;
-  }
-  .controle {
-    font-size: 1.2em;
-    margin-bottom: 20px;
+.controle label + input,
+.controle textarea {
+  width: 100%;
+  font-size: inherit;
+  border-radius: 5px;
+}
 
-  }
-  .controle label {
-    display: block;
-    font-weight: bold;
-  }
+.centralizado {
+  text-align: center;
+}
 
- .controle label + input, .controle textarea {
-    width: 100%;
-    font-size: inherit;
-    border-radius: 5px
-  }
-
-  .centralizado {
-    text-align: center;
-  }
-
+.erro {
+  color: rgb(230, 0, 0);
+}
 </style>
