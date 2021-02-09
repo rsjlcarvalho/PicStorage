d982f4424b8cc95980309907e5c7847e59b4f475
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2021-02-09 19:35:55 -0300

    Adding bootstrap;

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -9,6 +9,8 @@ import './directives/Transform';
 import VeeValidate from 'vee-validate';
 import msg from './pt_BR';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000';
 Vue.use(VueRouter);
